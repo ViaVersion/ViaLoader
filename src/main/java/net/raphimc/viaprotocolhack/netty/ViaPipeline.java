27b92b49ca0a12a20fe32d725e9c87bdb4f0838e
RaphiMC <50594595+RaphiMC@users.noreply.github.com>
2023-01-04 18:07:08 +0100

    Changed names of the via pipeline elements

--- src/main/java/net/raphimc/viaprotocolhack/netty/ViaPipeline.java
+++ src/main/java/net/raphimc/viaprotocolhack/netty/ViaPipeline.java
@@ -4,7 +4,7 @@ public class ViaPipeline {
 
     public static final String HANDLER_DECODER_NAME = "via-decoder";
     public static final String HANDLER_ENCODER_NAME = "via-encoder";
-    public static final String HANDLER_PRE_NETTY_ENCODER_NAME = "via-pre_netty-encoder";
-    public static final String HANDLER_PRE_NETTY_DECODER_NAME = "via-pre_netty-decoder";
+    public static final String HANDLER_PRE_NETTY_ENCODER_NAME = "via-pre-netty-encoder";
+    public static final String HANDLER_PRE_NETTY_DECODER_NAME = "via-pre-netty-decoder";
 
 }
