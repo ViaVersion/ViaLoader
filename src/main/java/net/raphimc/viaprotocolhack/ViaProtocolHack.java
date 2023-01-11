097e3fcf07f8ea6e51f09e37dc82ea4b37498d8a
RaphiMC <50594595+RaphiMC@users.noreply.github.com>
2023-01-11 20:52:23 +0100

    Added copyright headers

--- src/main/java/net/raphimc/viaprotocolhack/ViaProtocolHack.java
+++ src/main/java/net/raphimc/viaprotocolhack/ViaProtocolHack.java
@@ -1,16 +1,37 @@
+/*
+ * This file is part of ViaProtocolHack - https://github.com/RaphiMC/ViaProtocolHack
+ * Copyright (C) 2023 RK_01/RaphiMC and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package net.raphimc.viaprotocolhack;
 
 import com.viaversion.viabackwards.api.ViaBackwardsPlatform;
 import com.viaversion.viaversion.ViaManagerImpl;
 import com.viaversion.viaversion.api.Via;
 import com.viaversion.viaversion.api.data.MappingDataLoader;
-import com.viaversion.viaversion.api.platform.*;
+import com.viaversion.viaversion.api.platform.ViaInjector;
+import com.viaversion.viaversion.api.platform.ViaPlatform;
+import com.viaversion.viaversion.api.platform.ViaPlatformLoader;
 import com.viaversion.viaversion.commands.ViaCommandHandler;
 import com.viaversion.viaversion.protocol.ProtocolManagerImpl;
 import de.gerrygames.viarewind.api.ViaRewindPlatform;
 import net.raphimc.vialegacy.platform.ViaLegacyPlatform;
 import net.raphimc.viaprotocolhack.impl.platform.ViaVersionPlatformImpl;
-import net.raphimc.viaprotocolhack.impl.viaversion.*;
+import net.raphimc.viaprotocolhack.impl.viaversion.VPCommandHandler;
+import net.raphimc.viaprotocolhack.impl.viaversion.VPInjector;
+import net.raphimc.viaprotocolhack.impl.viaversion.VPLoader;
 import net.raphimc.viaprotocolhack.util.JLoggerToSLF4J;
 import org.slf4j.LoggerFactory;
 
