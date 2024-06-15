daddc6d675fee59d073282ef5587a598686c7fc3
FlorianMichael <florian.michael07@gmail.com>
2024-06-15 20:56:21 +0200

    Remove DontBugMe command from VLCommandHandler
    Removing this since the version checker isn't supported by ViaLoader anyway

--- src/main/java/net/raphimc/vialoader/impl/viaversion/VLCommandHandler.java
+++ src/main/java/net/raphimc/vialoader/impl/viaversion/VLCommandHandler.java
@@ -20,4 +20,9 @@ package net.raphimc.vialoader.impl.viaversion;
 import com.viaversion.viaversion.commands.ViaCommandHandler;
 
 public class VLCommandHandler extends ViaCommandHandler {
+
+    public VLCommandHandler() {
+        this.removeSubCommand("dontbugme");
+    }
+
 }
