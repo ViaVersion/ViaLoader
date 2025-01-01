094fac6123a8ea647f92d5c92bc063e5394444ef
RaphiMC <50594595+RaphiMC@users.noreply.github.com>
2025-01-01 01:33:29 +0100

    [ci skip] Updated copyright: 2025

--- src/main/java/net/raphimc/vialoader/commands/UserCommandSender.java
+++ src/main/java/net/raphimc/vialoader/commands/UserCommandSender.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of ViaLoader - https://github.com/RaphiMC/ViaLoader
- * Copyright (C) 2020-2024 RK_01/RaphiMC and contributors
+ * Copyright (C) 2020-2025 RK_01/RaphiMC and contributors
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
